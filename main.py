23e7f6406b9a9537d92b38ef64a6ae3d72e88c63
hainlh <longhai230303@gmail.com>
2023-12-21 14:39:03 +0700

    change

--- main.py
+++ main.py
@@ -252,7 +252,7 @@ def log_metrics(conf, model, metrics, log_path, checkpoint_model_path, checkpoin
 
     log = open(log_path, "a")
 
-    topk_ = 20 
+    topk_ = 3 
     print("top%d as the final evaluation standard" %(topk_))
     if metrics["val"]["recall"][topk_] > best_metrics["val"]["recall"][topk_] and metrics["val"]["ndcg"][topk_] > best_metrics["val"]["ndcg"][topk_]:
         # torch.save(model.state_dict(), checkpoint_model_path)
